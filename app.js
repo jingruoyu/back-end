fdb86abb0506966c9f2b944d52aa8a894990e90d
jingruoyu <jingruoyu@163.com>
2018-06-02 23:03:35 +0800

    app.js运行成功

--- app.js
+++ app.js
@@ -15,16 +15,17 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.static(__dirname + "/dist"));
 
 app.all('*', function(req, res, next){
+    var reg = /.*jingruoyu.com$|127.0.0.1/;
+    if(!reg.test(req.hostname)){
+        res.send('error hostname');
+        res.end();
+    }
     next();
 })
 
 app.get('/', function(req, res){
     console.log(req.hostname);
-//    if(req.hostname !== 'www.jingruoyu.com'){
-//        return;
-//    }
-	// res.send('this is a request')
-	console.log(__dirname)
+    console.log(__dirname)
     res.sendFile("/dist/index.html");
     res.end();
 })
